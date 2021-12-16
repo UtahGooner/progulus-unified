2f534c9a91ee97dac4a7e418b213d6f0787348bd
Steve Montgomery <steve@chums.com>
2021-12-16 12:59:26 -0700

    Remove unneeded files.

--- api/slored.php
+++ api/slored.php
@@ -1,3 +1,15 @@
 <?php
 
+require_once 'SamPDO.php';
+require_once 'autoload.inc.php';
+require_once 'json_utils.php';
+require_once 'CurrentSong.php';
+
+global $user;
+$userID = $user->data['user_id'];
+
+if (!$userID || $userID === 1) {
+    header('Location: /rprweb/');
+    exit;
+}
 phpinfo();
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location:/rprweb/#page-playing", true, 301);
-
